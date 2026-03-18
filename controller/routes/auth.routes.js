a752515bb7b64684198744e41a5fbaf7f1fb4aa5
BrentULYTuazon <164973803+BrentULYTuazon@users.noreply.github.com>
2026-03-19 02:19:09 +0800

    Update auth.routes.js
    Added New Routes

--- controller/routes/auth.routes.js
+++ controller/routes/auth.routes.js
@@ -1,11 +1,31 @@
+// controller/routes/auth.routes.js
 const express = require("express");
 const router = express.Router();
 
+// Homepage (Marketplace Feed)
 router.get('/', (req, res) => {
+    res.render('index', {
+        layout: 'main',
+        title: 'Animarket - DLSU Exclusive',
+        stylesheets: ['global.css', 'style.css']
+    });
+});
+
+// Login Page
+router.get('/login', (req, res) => {
     res.render('login', {
         layout: 'main',
-        title: 'Login - DLSU Marketplace',
-        stylesheets: ['global.css','style.css']
+        title: 'Login - Animarket',
+        stylesheets: ['global.css', 'style.css']
+    });
+});
+
+// Register Page
+router.get('/register', (req, res) => {
+    res.render('register', {
+        layout: 'main',
+        title: 'Register - Animarket',
+        stylesheets: ['global.css', 'style.css']
     });
 });
 
